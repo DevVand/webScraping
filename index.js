1a3303d210aea9df57b383e86b4350cbfabad6df
Vanderlei Catione <vanderleicatione32@gmail.com>
2020-08-04 20:21:08 -0300

    Adicionando puppeter

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hi!!")
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar');
+    await page.click('a[href$="padrao-tiss-junho-2020"]');
+    const links = await page.$$eval('a[href$="Padrao_TISS_Componente_Organizacional_202006.pdf"]', e=>e.map((a)=>a.href))
+    console.log(links[0])
+  })();
