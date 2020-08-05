a4128c605922e1f1f9c535a5e1af7e8899b355fb
Vanderlei Catione <vanderleicatione32@gmail.com>
2020-08-04 21:53:53 -0300

    Usando download-file para baixar o pdf

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');//Para navegar pelas paginas
+const download = require('download-file');//Para fazer o download
 
 (async () => {
+    //Cria a pagina
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+
+    //Navega pela pagina
     await page.goto('http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar');
+
+    //Busca o padrão tiss mais atualizado
     await page.click('a[href$="padrao-tiss-junho-2020"]');
-    const links = await page.$$eval('a[href$="Padrao_TISS_Componente_Organizacional_202006.pdf"]', e=>e.map((a)=>a.href))
-    console.log(links[0])
+
+     //Baixa o padrão tiss
+    const links = await page.$eval('a[href$="Padrao_TISS_Componente_Organizacional_202006.pdf"]',a=>a.href)
+    var options = {
+        directory: "",
+        filename: "Padrao_TISS_Componente_Organizacional_202006.pdf"
+    }
+    
+    download(links, options, function(err){
+        if (err) throw err
+        console.log("Pdf Baixado")
+    })
+    //Fecha o app
+    await browser.close();
   })();
+
+
